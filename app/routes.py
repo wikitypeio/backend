6631f8c919ac7b1934aaabf497b627aa0792aa84
Brian Travis <bztravis88@gmail.com>
2024-01-04 00:00:25 -0500

    Add cleaning article before text passage parsing

--- app/routes.py
+++ app/routes.py
@@ -5,23 +5,25 @@ from bs4 import BeautifulSoup, NavigableString
 import flask
 from . import app
 
-@app.route("/v1/article", methods=['GET'])
+@app.route('/v1/article', methods=['GET'])
 def get_article():
     '''Scrape, transform, and return specified article as array of letters'''
     article_url = flask.request.args.get('url')
     if article_url is None:
-        return {"message": "No url specified"}, 400
+        return {'message': 'No url specified'}, 400
 
-    res = requests.get(article_url, timeout=5)
-    soup = BeautifulSoup(res.text, 'html.parser')
-    article = soup.find('div', class_='mw-parser-output')
+    article_res = requests.get(article_url, timeout=5)
+    body = BeautifulSoup(article_res.text, 'html.parser')
+    article = body.find('div', id='mw-content-text')
 
-    clean_article(article)
+    response = {}
 
     # get image from infobox
-    info_box = article.find('table', class_="infobox")
-    primary_img = info_box.find('img')
-    primary_img_src = primary_img.attrs['src'].replace('//', '')
+    info_box = body.find('table', class_='infobox')
+    if info_box is not None:
+        primary_img = info_box.find('img')
+        primary_img_src = primary_img.attrs['src'].replace('//', '')
+        response['primaryImgSrc'] = primary_img_src
 
     # get all image sources and their captions
     images = []
@@ -30,26 +32,42 @@ def get_article():
         if img is None:
             continue
         src = img.attrs['src'].replace('//', '')    # srcs are prefixed with '//'
-        caption = figure.find("figcaption").get_text()
-        images.append({"src": src, "caption": caption})
+        caption = figure.find('figcaption').get_text()
+        images.append({'src': src, 'caption': caption})
+    response['images'] = images
 
+    # get text passage from article
+    clean_article(article)
     passage = recursive_append_text(article)
+    response['passage'] = passage
 
-    return {"images": images, "primaryImgSrc": primary_img_src, "passage": passage}
+    return response
 
 
 def clean_article(article):
-    '''Remove references'''
+    '''Remove references, h2, figures, notes, .infobox, .shortdescription, .mw-editsection'''
     for reference_elem in article.find_all(class_='reference'):
         reference_elem.decompose()
+    for heading in article.find_all('h2'):
+        heading.decompose()
+    for fig in article.find_all('figure'):
+        fig.decompose()
+    for note in article.find_all(attrs={'role': 'note'}):
+        note.decompose()
+    article.find(class_='infobox').decompose()
+    article.find(class_='shortdescription').decompose()
+    for edit in article.find_all(class_='mw-editsection'):
+        edit.decompose()
 
 
 def recursive_append_text(element, text=''):
     '''Get text and annotation ranges'''
     if isinstance(element, NavigableString) or len(element.contents) == 0:
-        return element.get_text()
+        return element.get_text().replace('\n', '')
 
     inner_text = ''
     for child in element.contents:
         inner_text += recursive_append_text(child, text)
+    # if element.name == 'p':
+        # inner_text += '\nabcdefghijklmnop\n'
     return inner_text
