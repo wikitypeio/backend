6a1d5fd7c7a79eee93816e390de75beae20fd3bd
Brian Travis <bztravis88@gmail.com>
2024-01-03 22:10:18 -0500

    Add basic MVP of article scrape: images, captions, main image, passage str

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
+'''API v1 routes'''
+
 import requests
 from bs4 import BeautifulSoup, NavigableString
 import flask
-import json
 from . import app
 
 @app.route("/v1/article", methods=['GET'])
@@ -11,16 +12,44 @@ def get_article():
     if article_url is None:
         return {"message": "No url specified"}, 400
 
-    try:
-        res = requests.get(article_url, timeout=5)
-        if res.status_code != 200:
-            raise ConnectionError("Error fetching article")
-    except requests.exceptions.ConnectTimeout as e:
-        print("TIMED OUT")
-        return {"message": f"{e}"}, 500
-    except ConnectionError as e:
-        return {"message": f"{e}"}, 500
+    res = requests.get(article_url, timeout=5)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    article = soup.find('div', class_='mw-parser-output')
+
+    clean_article(article)
+
+    # get image from infobox
+    info_box = article.find('table', class_="infobox")
+    primary_img = info_box.find('img')
+    primary_img_src = primary_img.attrs['src'].replace('//', '')
+
+    # get all image sources and their captions
+    images = []
+    for figure in article.find_all('figure'):
+        img = figure.find('img')
+        if img is None:
+            continue
+        src = img.attrs['src'].replace('//', '')    # srcs are prefixed with '//'
+        caption = figure.find("figcaption").get_text()
+        images.append({"src": src, "caption": caption})
+
+    passage = recursive_append_text(article)
+
+    return {"images": images, "primaryImgSrc": primary_img_src, "passage": passage}
 
 
+def clean_article(article):
+    '''Remove references'''
+    for reference_elem in article.find_all(class_='reference'):
+        reference_elem.decompose()
 
-    return {"message": "Hello world! Success!", "data": res.json()}
+
+def recursive_append_text(element, text=''):
+    '''Get text and annotation ranges'''
+    if isinstance(element, NavigableString) or len(element.contents) == 0:
+        return element.get_text()
+
+    inner_text = ''
+    for child in element.contents:
+        inner_text += recursive_append_text(child, text)
+    return inner_text
